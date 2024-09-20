24e524dbc9af318fa13e441aec188424a3c2c563
Kyle Franz <kyle@basisrobotics.tech>
2024-09-19 19:30:07 -0700

    fix some snags with docker + ros

--- docker/build-env.sh
+++ docker/build-env.sh
@@ -5,6 +5,14 @@ BASIS_SOURCE_DIR=${BASIS_SOURCE_DIR:-"$SCRIPT_DIR/../../basis"}
 #BASE_IMAGE=nvcr.io/nvidia/l4t-tensorrt:r8.6.2-devel
 BASE_IMAGE=nvcr.io/nvidia/l4t-jetpack:r36.3.0
 
-BASIS_ENABLE_ROS=0 ${BASIS_SOURCE_DIR}/docker/build-env.sh  --build-arg BASE_IMAGE=${BASE_IMAGE}
 
-docker build --tag basis-robot-env --target basis-robot-env -f $SCRIPT_DIR/../docker/Dockerfile $@ $SCRIPT_DIR/../
+BASIS_ENABLE_ROS=${BASIS_ENABLE_ROS:-0}
+
+BASIS_ENABLE_ROS=${BASIS_ENABLE_ROS} ${BASIS_SOURCE_DIR}/docker/build-env.sh  --build-arg BASE_IMAGE=${BASE_IMAGE}
+
+ADDITIONAL_ARGS=""
+if [ ${BASIS_ENABLE_ROS} -ne 0 ]; then
+    ADDITIONAL_ARGS="${ADDITIONAL_ARGS} --build-arg BASE_IMAGE=basis-env-ros"
+fi
+
+docker build --tag basis-robot-env --target basis-robot-env ${ADDITIONAL_ARGS} -f $SCRIPT_DIR/../docker/Dockerfile $@ $SCRIPT_DIR/../
