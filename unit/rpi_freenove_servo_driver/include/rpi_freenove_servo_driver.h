7a49fc799eb91b970e6d7fcf93c5cab1f2d1350a
Kyle Franz <kyle@basisrobotics.tech>
2024-11-26 18:33:09 -0800

    fix initialization

--- unit/rpi_freenove_servo_driver/include/rpi_freenove_servo_driver.h
+++ unit/rpi_freenove_servo_driver/include/rpi_freenove_servo_driver.h
@@ -14,11 +14,10 @@ public:
   virtual unit::rpi_freenove_servo_driver::Update::Output
   Update(const unit::rpi_freenove_servo_driver::Update::Input &input) override;
 
-
   PiPCA9685::PCA9685 pca;
 
   static inline constexpr size_t NUM_SERVOS = 2;
 
-  std::array<double, NUM_SERVOS> current_state;
-  std::array<double, NUM_SERVOS> requested_state;
+  std::array<double, NUM_SERVOS> current_state = {};
+  std::array<double, NUM_SERVOS> requested_state = {};
 };
